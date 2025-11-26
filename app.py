249df711ae7541ff52d769ae15737a59756192b0
omri zaher <omriza5@gmail.com>
2025-11-26 13:26:00 +0200

    add signal fetching functionality and limit API results to 10

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from dotenv import load_dotenv
+import requests
 import os
 
 load_dotenv() 
@@ -8,14 +9,20 @@ DEFAULT_INTELLIGENCE_ADJACENT = os.getenv("TARGET_DOMAIN", "bellingcat.com")
 DEFAULT_SIGNAL_FILE = os.getenv("SIGNAL_FILE", "data/signals.jsonl")
 DEFAULT_MAX_RECORDS = os.getenv("MAX_RECORDS", 100)
 
-INTELLIGENCE_ADJACENT_API = f"https://web.archive.org/cdx/search/cdx?url={DEFAULT_INTELLIGENCE_ADJACENT}/*&output=json"
+INTELLIGENCE_ADJACENT_API = f"https://web.archive.org/cdx/search/cdx?url={DEFAULT_INTELLIGENCE_ADJACENT}/*&output=json&limit=10"
 
-print(INTELLIGENCE_ADJACENT_API)
 app = Flask(__name__)
 
 @app.route('/health')
 def health():
     return "OK" , 200
+
+def get_signals():
+    response = requests.get(INTELLIGENCE_ADJACENT_API)
+    data = response.json()
+    
+
+# get_signals()
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
