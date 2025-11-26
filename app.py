37e93fb9d71a424f90fc74491394520b309cf292
omri zaher <omriza5@gmail.com>
2025-11-26 13:20:25 +0200

    add environment variable support and initialize signals data file

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from dotenv import load_dotenv
+import os
 
 load_dotenv() 
 
+DEFAULT_INTELLIGENCE_ADJACENT = os.getenv("TARGET_DOMAIN", "bellingcat.com")
+DEFAULT_SIGNAL_FILE = os.getenv("SIGNAL_FILE", "data/signals.jsonl")
+DEFAULT_MAX_RECORDS = os.getenv("MAX_RECORDS", 100)
+
+INTELLIGENCE_ADJACENT_API = f"https://web.archive.org/cdx/search/cdx?url={DEFAULT_INTELLIGENCE_ADJACENT}/*&output=json"
+
+print(INTELLIGENCE_ADJACENT_API)
 app = Flask(__name__)
 
 @app.route('/health')
