88d9a23fc522b0da37798255720ef5344881fbe7
omri zaher <omriza5@gmail.com>
2025-11-26 15:30:01 +0200

    Update environment variables and use APP_PORT in app.run

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ load_dotenv()
 INTELLIGENCE_ADJACENT = os.getenv("TARGET_DOMAIN", "bellingcat.com")
 SIGNAL_FILE = os.getenv("SIGNAL_FILE", "data/signals.jsonl")
 MAX_RECORDS = os.getenv("MAX_RECORDS", 100)
+APP_PORT= os.getenv("APP_PORT", 8000)
 
 INTELLIGENCE_ADJACENT_API = f"https://web.archive.org/cdx/search/cdx?url={INTELLIGENCE_ADJACENT}/*&output=json&limit=10"
 
@@ -144,4 +145,4 @@ def extract_protocol_part(url):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=APP_PORT)
